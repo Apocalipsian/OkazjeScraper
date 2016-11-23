51b15b84e4ef5b13ec0af103c71563cc6b4f670d
okazje <okazje@DESKTOP-N28LJ22>
2016-11-23 15:25:44 +0100

    Add project files.

---
+++ ShopCountProject/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace ShopCountProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleSite = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.compareTrunk = new System.Windows.Forms.CheckBox();
+            this.trunkUrl = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkListing = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkLf = new System.Windows.Forms.RadioButton();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // simpleSite
+            // 
+            this.simpleSite.AutoSize = true;
+            this.simpleSite.Checked = true;
+            this.simpleSite.Location = new System.Drawing.Point(6, 19);
+            this.simpleSite.Name = "simpleSite";
+            this.simpleSite.Size = new System.Drawing.Size(112, 17);
+            this.simpleSite.TabIndex = 1;
+            this.simpleSite.TabStop = true;
+            this.simpleSite.Text = "Pojedyncza strona";
+            this.simpleSite.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(102, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Pierwsze 5 stron";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(31, 171);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(473, 353);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // compareTrunk
+            // 
+            this.compareTrunk.AutoSize = true;
+            this.compareTrunk.Location = new System.Drawing.Point(12, 104);
+            this.compareTrunk.Name = "compareTrunk";
+            this.compareTrunk.Size = new System.Drawing.Size(124, 17);
+            this.compareTrunk.TabIndex = 4;
+            this.compareTrunk.Text = "Porównaj z trunkiem ";
+            this.compareTrunk.UseVisualStyleBackColor = true;
+            // 
+            // trunkUrl
+            // 
+            this.trunkUrl.Location = new System.Drawing.Point(142, 103);
+            this.trunkUrl.Name = "trunkUrl";
+            this.trunkUrl.Size = new System.Drawing.Size(296, 20);
+            this.trunkUrl.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 539);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(473, 48);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Kris testuje";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(528, 483);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 41);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Zapisz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(528, 434);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 43);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Otwórz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // checkListing
+            // 
+            this.checkListing.AutoSize = true;
+            this.checkListing.Checked = true;
+            this.checkListing.Location = new System.Drawing.Point(15, 19);
+            this.checkListing.Name = "checkListing";
+            this.checkListing.Size = new System.Drawing.Size(67, 17);
+            this.checkListing.TabIndex = 10;
+            this.checkListing.TabStop = true;
+            this.checkListing.Text = "LISTING";
+            this.checkListing.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkLf);
+            this.groupBox1.Controls.Add(this.checkListing);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 70);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.simpleSite);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Location = new System.Drawing.Point(238, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 70);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // checkLf
+            // 
+            this.checkLf.AutoSize = true;
+            this.checkLf.Location = new System.Drawing.Point(15, 42);
+            this.checkLf.Name = "checkLf";
+            this.checkLf.Size = new System.Drawing.Size(37, 17);
+            this.checkLf.TabIndex = 13;
+            this.checkLf.TabStop = true;
+            this.checkLf.Text = "LF";
+            this.checkLf.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(630, 53);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(347, 345);
+            this.richTextBox2.TabIndex = 13;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 687);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trunkUrl);
+            this.Controls.Add(this.compareTrunk);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton simpleSite;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox compareTrunk;
+        private System.Windows.Forms.TextBox trunkUrl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RadioButton checkListing;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton checkLf;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ ShopCountProject/Form1.cs
@@ -0,0 +1,378 @@
+﻿/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@FreeBSD.ORG> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer.  
+ * ----------------------------------------------------------------------------
+ */
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.PhantomJS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopCountProject
+{
+    public partial class Form1 : Form
+    {
+
+        static IWebDriver driver;
+        static string tittle = "";
+        static List<string> lista = new List<string>();
+        static StringBuilder csvWrite = new StringBuilder();
+        static public List<string> productIdList = new List<string>();
+        public static List<string> listBox = new List<string>();
+        public List<List<string>> oneList = new List<List<string>>();
+
+        public List<string> getDuplicateProduct(List<string> listProductId)
+        {
+            listProductId = driver.FindElements(By.XPath(
+                   @"//div[@id='listingPage']//div[contains(@class,'rightCol')]/div[contains(@class,'squaresListing')]
+                    /div[contains(@class, 'squaresContent narrow')]/div[contains(@class, 'productItem')]//a[contains(@class,'productItemContent')]")).Select(w => w.GetAttribute("data_product_id")).ToList();
+
+            var duplicate = listProductId.GroupBy(x => x)
+                .Where(g => g.Count() > 1)
+                .Select(y => y.Key)
+                .ToList();
+
+            return duplicate;
+        }
+
+        public static List<string> checkShop(List<string> lista, string url)
+        {
+            lista = new List<string>();
+            if (driver == null)
+                driver = new PhantomJSDriver();
+            driver.Url = url;
+
+
+            //string cookieName = driver.Manage().Cookies.GetCookieNamed("OIATM").Value.ToString();
+            //Cookie cookie = new Cookie("OIATM", "8af3%3A1%3A1");
+            //Cookie cookie2 = new Cookie("OIATM", "8af3%3A2%3A1");
+
+            //driver.Manage().Cookies.DeleteCookieNamed("OIATM");
+            //driver.Manage().Cookies.AddCookie(cookie);
+
+            //driver.Url = url;
+
+            //driver.Manage().Cookies.DeleteCookieNamed("OIATM");
+            //driver.Manage().Cookies.AddCookie(cookie2);
+
+            driver.Url = url;
+
+            tittle = driver.Title;
+            if (url.Contains("okazje.info.pl") == false)
+            {
+                tittle += " - trunk";
+            }
+            tittle = tittle.Replace("najlepsze ceny", DateTime.Now.ToString("HH_mm_ss"));
+
+
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(1));
+            var la = driver.FindElements(By.XPath(
+                @"//div[@id='listingPage']//div[contains(@class,'rightCol')]/div[contains(@class,'squaresListing')]
+                     /div[contains(@class,'squaresContent narrow')]/div[contains(@class,'productItem')]//a[contains(@class,'productItemContent')]
+                     /div[contains(@class, 'productPricesFieldArea')]/div[contains(@class, 'productShopName') or contains (@class,'orangeTex productOffersCount') ]"));
+
+
+            foreach (var item in la)
+            {
+                lista.Add(item.Text);
+            }
+
+
+            sortList(lista, 1);
+            if (Directory.Exists(@".\" + DateTime.Today.ToString("dd_MM_yyyy")) == false)
+                Directory.CreateDirectory(@".\" + DateTime.Today.ToString("dd_MM_yyyy"));
+
+            File.WriteAllText(@".\" + @".\" + DateTime.Today.ToString("dd_MM_yyyy") + @"\" + tittle + ".cvs", csvWrite.ToString());
+            csvWrite.Clear();
+
+            return lista;
+
+        }
+
+
+        public static List<string> checkShop(List<string> listaAll, string url, int countWeb)
+        {
+            if (listaAll == null)
+                listaAll = new List<string>();
+
+            List<string> lista = new List<string>();
+
+            if (driver == null)
+                driver = new PhantomJSDriver();
+
+            driver.Url = url;
+            tittle = driver.Title;
+            if (url.Contains("okazje.info.pl") == false)
+            {
+                tittle += " - trunk";
+            }
+            tittle = tittle.Replace("najlepsze ceny", DateTime.Now.ToString("HH_mm_ss"));
+
+            var count = driver.FindElements(By.XPath("//div[@id='listingPageArea']/div[@id='listingPage']/div[@class='rightCol overflowH']/div[@class='yu']/div/div[@class='wholePages']/a"));
+
+            if (count.Count >= 5)
+                countWeb = 5;
+            else
+                countWeb = count.Count + 1;
+
+            for (int i = 1; i <= countWeb; i++)
+            {
+                driver.Url = url + i + ".html";
+                driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(1));
+                var la = driver.FindElements(By.XPath(
+                    @"//div[@id='listingPage']//div[contains(@class,'rightCol')]/div[contains(@class,'squaresListing')]
+                     /div[contains(@class,'squaresContent narrow')]/div[contains(@class,'productItem')]//a[contains(@class,'productItemContent')]
+                     /div[contains(@class, 'productPricesFieldArea')]/div[contains(@class, 'productShopName') or contains (@class,'orangeTex productOffersCount') ]"));
+
+
+                var productID = driver.FindElements(By.XPath(
+                   @"//div[@id='listingPage']//div[contains(@class,'rightCol')]/div[contains(@class,'squaresListing')]
+                    /div[contains(@class, 'squaresContent narrow')]/div[contains(@class, 'productItem')]//a[contains(@class,'productItemContent')]")).Select(w => w.GetAttribute("data_product_id"));
+
+
+                foreach (var item in la)
+                {
+                    lista.Add(item.Text);
+                    listaAll.Add(item.Text);
+                }
+
+                foreach (var item in productID)
+                {
+                    productIdList.Add(item);
+                }
+
+  
+                    sortList(lista, 1);
+
+
+                lista = new List<string>();
+
+            }
+
+
+            sortList(listaAll, 0);
+
+
+            //        var topCategory_id = driver.FindElements(By.XPath(
+            //           @"//div[@id='listingPage']//div[contains(@class,'rightCol')]/div[contains(@class,'squaresListing')]
+            //                / div[contains(@class, 'squaresContent narrow')]/div[contains(@class, 'productItem')]//a[contains(@class,'productItemContent')]")).Select(w => w.GetAttribute("data_topcategory_id"));
+
+            //        var category_id = driver.FindElements(By.XPath(
+            //          @"//*[@id='categoryId']")).Select(w => w.GetAttribute("value"));
+
+            //        string solrUrl = "http://biuro:9983/solr/collection1/select?qt=standard&q=*:*&fq=topcategory_id:"+topCategory_id.FirstOrDefault().ToString()+"&fq=+categories:"+category_id.FirstOrDefault().ToString()+"&fq=+status:1&fl=id,topcategory_id&facet=true&facet.field=price_range&facet.mincount=5&facet.limit=-1&facet.sort=true&facet.field=p_1&facet.mincount=5&facet.limit=90&facet.sort=true&facet.field=p_237&facet.mincount=5&facet.limit=90&facet.sort=true&facet.field=p_238&facet.mincount=5&facet.limit=-1&facet.sort=true&sort=pretty_sort%20desc,%20price%20asc&json.nl=map&start=240&rows=480000&indent=1";
+
+
+            //        driver.Url = solrUrl;
+
+            //        var allProduct = driver.FindElements(By.XPath("response/result/doc/int[@name='id']")).Select(w => w.Text).ToList();
+
+
+            //     //   var duplicateProduct = productIdList.Where(b => allProduct.Any(a => b.Contains(a)));
+
+            //  //var tmp = productIdList.Where(b => allProduct.Any(a => b.Contains(a)))
+            //  //                   .Select(x => new { x, index = allProduct.IndexOf(x) });
+
+
+
+
+            //        var result = allProduct.Select((x, y) => new { element = x, index = y })  
+            //.Where(z => productIdList.Contains(z.element));
+
+
+            //        var tmp = result.Count();
+            //// na plikach
+            if (Directory.Exists(@".\" + DateTime.Today.ToString("dd_MM_yyyy")) == false)
+                Directory.CreateDirectory(@".\" + DateTime.Today.ToString("dd_MM_yyyy"));
+
+            File.WriteAllText(@".\" + @".\" + DateTime.Today.ToString("dd_MM_yyyy") + @"\" + tittle + ".cvs", csvWrite.ToString());
+            csvWrite.Clear();
+            return lista;
+
+        }
+
+        public static void sortList(List<string> lista, int lastPage)
+        {
+            // csvWrite.Clear();
+
+            //  csvWrite.AppendLine(driver.Url);
+
+            string tmp = "";
+
+            var numberMulti = from c in lista
+                              where c.Contains("ofert")
+
+                              select new { c };
+
+            int countTmp = numberMulti.Count();
+
+            lista.RemoveAll(u => u.Contains("ofert"));
+
+
+            var q = from x in lista
+
+                    group x by x into g
+                    let count = g.Count()
+                    orderby count descending
+                    select new { Value = g.Key, Count = count };
+
+
+
+            foreach (var x in q)
+            {
+                if (lastPage == 0)
+                {
+                    var newLine = string.Format("TOTAL;{0};{1};", x.Value, x.Count);
+                    csvWrite.AppendLine(newLine);
+                    tmp = "TOTAL";
+                }
+                else
+                {
+                    var newLine = string.Format("{0};{1};{2};", driver.Url, x.Value, x.Count);
+                    csvWrite.AppendLine(newLine);
+                    tmp = driver.Url;
+                }
+            }
+
+
+
+            var newLine2 = string.Format("{0};Produkty wielofertowe;{1}", tmp, countTmp);
+            csvWrite.AppendLine(newLine2);
+
+
+            var newLine3 = string.Format("{0};Ilość produktów; {1}", tmp, (lista.Count + countTmp));
+            csvWrite.AppendLine(newLine3);
+
+
+            var newLine4 = string.Format("{0};Ilość sklepów; {1}", tmp, (q.Count()));
+            csvWrite.AppendLine(newLine4);
+
+
+            csvWrite.AppendLine(Environment.NewLine);
+
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region Listing 
+            if (checkListing.Enabled)
+            {
+                if (richTextBox1.Lines.Length > 0)
+                {
+                    listBox.AddRange((string[])richTextBox1.Lines);
+
+                    if (simpleSite.Checked)
+                    {
+                        foreach (var url in listBox)
+                        {
+                            lista = checkShop(lista, url);
+                            lista = new List<string>();
+
+                            if (compareTrunk.Checked)
+                            {
+                                driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(120));
+
+                                lista = checkShop(lista, url.Replace("www.okazje.info.pl", trunkUrl.Text));
+                                lista = new List<string>();
+                            }
+                        }
+                        driver.Quit();
+                        driver = null;
+                        listBox = new List<string>();
+                    }
+
+                    if (radioButton2.Checked)
+                    {
+                        foreach (var url in listBox)
+                        {
+                            lista = checkShop(lista, url, 5);
+                            lista = new List<string>();
+
+
+                            if (compareTrunk.Checked)
+                            {
+                                driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(120));
+
+                                lista = checkShop(lista, url.Replace("www.okazje.info.pl", trunkUrl.Text), 5);
+                                lista = new List<string>();
+                            }
+
+                        }
+                        driver.Quit();
+                        driver = null;
+                        listBox = new List<string>();
+                    }
+                }
+                else
+                    MessageBox.Show("Podaj prawidłowe urle");
+
+            }
+            #endregion
+
+            #region LF
+
+            if (checkLf.Enabled)
+            {
+                foreach (var url in listBox)
+                {
+                    if (driver == null)
+                        driver = new PhantomJSDriver();
+
+                    driver.Url = url;
+                    List<string> duplicate = new List<string>();
+                    duplicate = getDuplicateProduct(duplicate);
+
+                    richTextBox2.Text += "Ilosc duplikatów : " + duplicate.Count();
+
+
+                }
+                listBox = new List<string>();
+                //driver.Quit();
+            }
+
+            #endregion
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Text|*.txt";
+            saveFileDialog1.Title = "Zapisz urle";
+            saveFileDialog1.ShowDialog();
+
+            richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text|*.txt";
+            openFile.Title = "Otwórz urle";
+            openFile.ShowDialog();
+
+            richTextBox1.LoadFile(openFile.FileName, RichTextBoxStreamType.PlainText);
+        }
+    }
+}
